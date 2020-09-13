cc0310d230edf14a8b987ea0a801d8575e2a569f
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-13 17:23:10 +1000

    added styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <script src="{{asset('js/app.js')}}" defer></script>
 
 </head>
