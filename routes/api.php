590feaf22ebe440cb16783cdcec0f5f32c723026
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-14 23:50:53 +1000

    api request findOrFail

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('bookables', function (Request $request) {
     return Bookable::all();
 });
+
+Route::get('bookables/{id}', function (Request $request, $id) {
+    return Bookable::findOrFail($id); //If noting find return 400
+    // return Bookable::find($id);//If noting find return 200 OK
+});
+
+// Route::get('bookables/{id}/{optional?}', function (Request $request, $id, $optional = "Jhon") {
+//     dd($id, $optional);
+//     return Bookable::find($id);
+// });
