1a7802796b16a71012a1051559bf64da0c9a093a
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-15 18:26:26 +1000

    added v-model with input

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -9,17 +9,33 @@
           name="from"
           class="from-control from-control-sm"
           placeholder="Start date"
+          v-model="from"
         />
       </div>
       <div class="form-group col-md-6">
         <label for="to">To</label>
-        <input type="text" name="to" class="from-control from-control-sm" placeholder="End date" />
+        <input
+          type="text"
+          name="to"
+          class="from-control from-control-sm"
+          placeholder="End date"
+          v-model="to"
+        />
       </div>
     </div>
     <button class="btn btn-secondary btn-block">Check!</button>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      from: null,
+      to: null,
+    };
+  },
+};
+</script>
 <style scoped>
 label {
   font-size: 0.7rem;
