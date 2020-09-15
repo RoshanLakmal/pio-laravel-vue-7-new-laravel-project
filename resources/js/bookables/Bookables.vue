d91bd4b9d4beb627bdc54715e798914d5e3e02eb
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-15 14:26:53 +1000

    v-bind passing prop object

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -8,11 +8,7 @@
           v-for="(bookable,columns) in bookablesInRow(row)"
           :key="'row'+row+columns"
         >
-          <bookable-list-item
-            :item-title="bookable.title"
-            :item-description="bookable.description"
-            :price="1000"
-          ></bookable-list-item>
+          <bookable-list-item v-bind="bookable"></bookable-list-item>
         </div>
         <!-- {{placeholdersInRow(row)}} -->
         <div class="col" v-for="p in placeholdersInRow(row)" :key="'placeholder'+row+p"></div>
