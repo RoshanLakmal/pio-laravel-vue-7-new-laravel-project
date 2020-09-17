13f23dc2771e05b8f6079bef742fad741010b983
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-17 18:52:33 +1000

    seed bookings

---
+++ database/seeders/BookingsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Bookable;
+use App\Models\Booking;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bookable::all()->each(function (Bookable $bookable) {
+            $booking = Booking::factory()->make();
+            $bookings = collect([$booking]);
+            for ($i = 0; $i < random_int(1, 20); $i++) {
+                $from = (clone $booking->to)->addDays(random_int(1, 14));
+                $to = (clone $from)->addDays(random_int(0, 14));
+
+                $booking = Booking::make([
+                    'from' => $from,
+                    'to' => $to,
+                    'bookable_id' => $bookable->id
+                ]);
+                $booking->push($booking);
+            }
+            $bookable->bookings()->saveMany($bookings);
+        });
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([BookablesTableSeeder::class]);
+        $this->call([BookingsTableSeeder::class]);
         // User::factory(10)->create();
     }
 }
