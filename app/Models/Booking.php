648af854387f48a11b4e1f4846f05c444dce333f
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-18 11:58:11 +1000

    added relationships between bookable, booking and review

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -14,7 +14,10 @@ class Booking extends Model
     {
         return $this->belongsTo(Bookable::class);
     }
-
+    public function review()
+    {
+        return $this->hasOne(Review::class);
+    }
     public function scopeBetweenDates(Builder $query, $from, $to)
     {
         return $query->where('to', '>', $from)->where('from', '<', $to);
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,6 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    public function bookable()
+    {
+        return $this->belongsTo(Bookable::class);
+    }
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
     public function getIncrementing()
     {
         return false;
