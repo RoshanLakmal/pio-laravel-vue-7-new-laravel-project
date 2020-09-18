1880e4140db6d88dd25ae4c1e7a7d746c67e4277
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-18 13:27:35 +1000

    added vue filters and moment js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ require("./bootstrap");
 import VueRouter from "vue-router";
 import router from "./routes";
 import Index from "./Index";
+import moment from "moment";
+
 window.Vue = require("vue");
 // Vue.component(
 //     "example-component",
@@ -12,6 +14,8 @@ window.Vue = require("vue");
 // Vue.component("example-2", require("./components/Example2.vue").default);
 
 Vue.use(VueRouter);
+
+Vue.filter("fromNow", value => moment(value).fromNow());
 
 const app = new Vue({
     el: "#app",
--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -1,42 +1,58 @@
 <template>
-  <div style="padding: 1.25rem">
-    <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">Review List Event</h6>
+    <div style="padding: 1.25rem">
+        <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">
+            Review List Event
+        </h6>
 
-    <div v-if="loading">loading...</div>
-    <div v-else>
-      <div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">
-        <div class="row pt-4">
-          <div class="col-md-6">Roshan</div>
-          <div class="col-md-6 d-flex justify-content-end">{{review.rating}}</div>
+        <div v-if="loading">loading...</div>
+        <div v-else>
+            <div
+                class="border-bottom d-none d-md-block"
+                v-for="(review, index) in reviews"
+                :key="index"
+            >
+                <div class="row pt-4">
+                    <div class="col-md-6">Roshan</div>
+                    <div class="col-md-6 d-flex justify-content-end">
+                        {{ review.rating }}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        {{ review.created_at | fromNow }}
+                    </div>
+                </div>
+                <div class="row pt-4 pd-4">
+                    <div class="col-md-12">{{ review.content }}</div>
+                </div>
+            </div>
         </div>
-        <div class="row">
-          <div class="col-md-12">{{review.created_at}}</div>
-        </div>
-        <div class="row pt-4 pd-4">
-          <div class="col-md-12">{{review.content}}</div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
+// import moment from "moment";
 export default {
-  props: {
-    bookableId: String,
-  },
-  data() {
-    return {
-      loading: false,
-      reviews: null,
-    };
-  },
-  created() {
-    this.loading = true;
-    axios
-      .get(`/api/bookables/${this.bookableId}/reviews`)
-      .then((response) => (this.reviews = response.data.data))
-      .then(() => (this.loading = false));
-  },
+    props: {
+        bookableId: String
+    },
+    data() {
+        return {
+            loading: false,
+            reviews: null
+        };
+    },
+    created() {
+        this.loading = true;
+        axios
+            .get(`/api/bookables/${this.bookableId}/reviews`)
+            .then(response => (this.reviews = response.data.data))
+            .then(() => (this.loading = false));
+    }
+    // filters: {
+    //     fromNow(value) {
+    //         return moment(value).fromNow();
+    //     }
+    // }
 };
 </script>
