e6cedb02ff3fd75f3c31324842a911e7ea2f8b79
Roshan Elvitigala <roshanrox911@gmail.com>
2020-09-24 14:06:13 +1000

    refactoring price calculator

--- app/Http/Controllers/Api/BookablePriceController.php
+++ app/Http/Controllers/Api/BookablePriceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bookable;
-use Carbon\Carbon;
+// use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BookablePriceController extends Controller
@@ -24,15 +24,16 @@ class BookablePriceController extends Controller
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
 
-        $days = (new Carbon($data['from']))->diffInDays(new Carbon($data['to'])) + 1;
-        $price = $days * $bookable->price;
+        // $days = (new Carbon($data['from']))->diffInDays(new Carbon($data['to'])) + 1;
+        // $price = $days * $bookable->price;
         return response()->json([
-            'data' => [
-                'total' => $price,
-                'breakdown' => [
-                    $bookable->price => $days
-                ]
-            ]
+            'data' => $bookable->priceFor($data['from'], $data['to'])
+            // 'data' => [
+            //     'total' => $price,
+            //     'breakdown' => [
+            //         $bookable->price => $days
+            //     ]
+            // ]
         ]);
     }
 }
--- app/Models/Bookable.php
+++ app/Models/Bookable.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Bookable extends Model
 {
@@ -19,5 +20,17 @@ class Bookable extends Model
     public function availableFor($from, $to): bool
     {
         return 0 === $this->bookings()->betweenDates($from, $to)->count();
+    }
+
+    public function pricefor($from, $to): array
+    {
+        $days = (new Carbon($from))->diffInDays(new Carbon($to)) + 1;
+        $price = $days * $this->price;
+        return [
+            'total' => $price,
+            'breakdown' => [
+                $this->price => $days
+            ]
+        ];
     }
 }
